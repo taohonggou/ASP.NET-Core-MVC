411d763863d8bb0df3ce5231d46562d022dca417
taohonggou <cl889521@163.com>
2017-06-06 17:23:32 +0800

    https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/first-mvc-app/working-with-sql

--- ASP.NET Core/Controllers/MoviesController.cs
+++ ASP.NET Core/Controllers/MoviesController.cs
@@ -26,7 +26,7 @@ namespace ASP.NET_Core.Controllers
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
-        {
+        { 
             if (id == null)
             {
                 return NotFound();
